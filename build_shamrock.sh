d1734015f5ed610b940df79d21e58683b5ca0672
Adil Aytan <adilaytanofficial@gmail.com>
2026-08-14 21:24:04 +0300

    Deleted comment lines

--- build_shamrock.sh
+++ build_shamrock.sh
@@ -1,7 +1,3 @@
-#rm -rf out/
-#mkdir -p out/target/product/shamrock/root
-#mkdir -p out/target/product/shamrock/recovery/root/system/etc
-
 export ALLOW_MISSING_DEPENDENCIES=true 
 export FOX_BUILD_DEVICE=shamrock
 export LC_ALL="C"
